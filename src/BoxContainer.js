60d3b31c6e6d4f21da40b418d8728c5dbeb42572
yosef <yosef.sugiarto@gmail.com>
2020-12-31 10:53:47 +0700

    feat: added box container component

--- src/BoxContainer.js
+++ src/BoxContainer.js
@@ -1,46 +1,35 @@
 import React from 'react';
-import {
-    View,
-    Text
-} from 'react-native';
+import { View, Text } from 'react-native';
 
-import 
-    EStyleSheet 
-from 'react-native-extended-stylesheet';
+import EStyleSheet from 'react-native-extended-stylesheet';
 
 const styles = EStyleSheet.create({
-    mainContainer: {
-        padding: "16rem",
-    },
-    titleContainer: {
-        borderBottomWidth: 1,
-        borderBottomColor: '#F1F1F1',
-        paddingBottom: "14rem"
-    },
-    title: {
-        fontSize: "16rem",
-        fontWeight: '700',
-        lineHeight: "21.89rem"
-    }
-})
+  mainContainer: {
+    padding: '16rem',
+  },
+  titleContainer: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#F1F1F1',
+    paddingBottom: '14rem',
+  },
+  title: {
+    fontSize: '16rem',
+    fontWeight: '700',
+    lineHeight: '21.89rem',
+  },
+});
 
+const BoxContainer = ({ title, titleChild, children }) => {
+  return (
+    <View style={styles.mainContainer}>
+      <View style={styles.titleContainer}>
+        {titleChild !== undefined && titleChild}
 
-const BoxContainer = ({title, titleChild ,children}) =>{
-    return (
-        <View style={styles.mainContainer}>
-            <View style={styles.titleContainer}>
-            { titleChild !== undefined && titleChild }    
+        {title !== undefined && <Text style={styles.title}>{title}</Text>}
+      </View>
+      <View>{children}</View>
+    </View>
+  );
+};
 
-            { title !== undefined && (
-                <Text style={styles.title}>{title}</Text>
-            )}    
-            </View>
-            <View>
-                {children}
-            </View>
-        </View>
-    )
-}
-
-export default BoxContainer
-
+export default BoxContainer;
