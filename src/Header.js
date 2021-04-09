cac52a1eeb324f55a0e8e86c6067de2f0bbbaada
yosef <yosef.sugiarto@gmail.com>
2021-04-10 04:42:39 +0700

    chore: header component

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import { View, Text, TouchableWithoutFeedback, Dimensions } from 'react-native';
+import {
+  View,
+  Text,
+  TouchableWithoutFeedback,
+  Dimensions,
+  Pressable,
+} from 'react-native';
 
 import Icon from 'react-native-vector-icons/AntDesign';
 
@@ -52,109 +58,65 @@ const Header = ({
 
   return (
     <View
+      style={[styles.container, style]}
       onLayout={(e) => onLayout && onLayout(e)}
-      style={[
-        {
-          backgroundColor: 'white',
-        },
-        style,
-      ]}
     >
-      <View style={[styles.container, style]}>
-        <View
-          style={{
-            flexDirection: 'row',
-            backgroundColor: 'transparent',
+      <View
+        style={{
+          backgroundColor: 'transparent',
+          flexDirection: 'row',
+          flex: 1,
+        }}
+      >
+        <Pressable
+          onPress={() => {
+            navigation && navigation.goBack();
+            action && action();
           }}
+          style={[
+            {
+              flex: 0.15,
+              backgroundColor: 'transparent',
+              flexDirection: 'row',
+              alignItems: 'center',
+            },
+            leftContainerStyle,
+          ]}
         >
-          {action !== undefined && navigation == undefined && (
-            <View
-              style={[
-                {
-                  flex: 0.15,
-                  backgroundColor: 'transparent',
-                  flexDirection: 'row',
-                  alignItems: 'center',
-                },
-                leftContainerStyle,
-              ]}
-            >
-              <TouchableWithoutFeedback onPress={() => action()}>
-                {_leftIcon}
-              </TouchableWithoutFeedback>
-            </View>
+          {_leftIcon}
+        </Pressable>
+
+        <View
+          style={[
+            {
+              flex: 1,
+              backgroundColor: 'transparent',
+              alignItems: 'center',
+              flexDirection: 'row',
+            },
+            centerContainerStyle,
+          ]}
+        >
+          {typeof children === 'string' && (
+            <Text style={[styles.title, { color: titleColor }]}>
+              {children}
+            </Text>
           )}
 
-          {navigation !== undefined && action == undefined && (
-            <View
-              style={[
-                {
-                  flex: 0.15,
-                  backgroundColor: 'transparent',
-                  flexDirection: 'row',
-                  alignItems: 'center',
-                },
-                leftContainerStyle,
-              ]}
-            >
-              <TouchableWithoutFeedback
-                onPress={() => {
-                  navigation.goBack();
-                }}
-              >
-                {_leftIcon}
-              </TouchableWithoutFeedback>
-            </View>
-          )}
-
-          <View
-            style={[
-              {
-                flex: 1,
-                backgroundColor: 'transparent',
-                alignItems: 'center',
-                flexDirection: 'row',
-              },
-              centerContainerStyle,
-            ]}
-          >
-            {typeof children === 'string' && (
-              <Text style={[styles.title, { color: titleColor }]}>
-                {children}
-              </Text>
-            )}
-
-            {typeof children !== 'string' && children}
-          </View>
-
-          {rightIcon !== undefined && (
-            <View
-              style={[
-                {
-                  flex: 0.15,
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  backgroundColor: 'transparent',
-                },
-                rightContainerStyle,
-              ]}
-            >
-              {rightIcon}
-            </View>
-          )}
-
-          {navigation !== undefined && rightIcon === undefined && (
-            <View
-              style={{
-                flex: 0.15,
-                justifyContent: 'center',
-                alignItems: 'center',
-                backgroundColor: 'transparent',
-              }}
-            >
-              <Icon size={24} color={'transparent'} name="left" />
-            </View>
-          )}
+          {typeof children !== 'string' && children}
+        </View>
+        <View
+          style={[
+            {
+              flex: 0.15,
+              justifyContent: 'center',
+              alignItems: 'center',
+              backgroundColor: 'transparent',
+            },
+            rightContainerStyle,
+          ]}
+        >
+          {rightIcon !== undefined && rightIcon}
         </View>
       </View>
     </View>
